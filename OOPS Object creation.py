4fbb238d2d4c6e68029a3c652e004f8249c565ac
jadhav Naik <86229588+Jadhavnandu@users.noreply.github.com>
2023-01-04 21:21:59 +0530

    Add files via upload

---
+++ OOPS Object creation.py
@@ -0,0 +1,31 @@
+class student:
+    # noinspection PyUnreachableCode
+    def check_pass_fail(self):
+        if self.marks >= 50:
+            return True
+        else:
+            return False
+
+
+def __init__(self, name, marks):
+    self.name = name
+    self.marks = marks
+    student1 = student("nandu", 85)
+    student2 = student("nayak", 40)
+    print(student1.name)
+    print(student1.marks)
+    print(student2.name)
+    print(student2.marks)
+
+
+# Checkig the S1 and S2
+    did_pass = student1.check_pass_fail()
+    print(did_pass)
+    did_pass = student2.check_pass_fail()
+    print(did_pass)
+
+
+
+
+
+##  I have to complete exam today if any cost.
